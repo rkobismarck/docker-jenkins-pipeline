a841ef00c053610aadf1d0e5200a9f75e43148b5
Roberto Carlos Trujillo Torres <robertotrujillo@MacBook-Pro-de-Roberto.local>
2018-11-05 21:31:19 -0600

    Improved configuration file containing the AWS instances

--- AWS/instances.sh
+++ AWS/instances.sh
@@ -6,16 +6,19 @@ if [ "$1" != "" ]; then
 
     if [ "$1" == "stop" ]; then
     	COMMAND="stop-instances"
-	fi	
-
-	echo  "$COMMAND Jenkins Server"
-	aws ec2 $COMMAND --instance-ids i-042d48d762598044a
-	
-	echo "$COMMAND Docker Server"
-	aws ec2 $COMMAND --instance-ids i-0d2e4dce957337587
-	
+	fi		
 else
     echo "No valid argument was supplied, please try again with: start, stop."
     exit
 fi
 
+if [ "$2" != "" ] && [ -e "$2" ] ; then
+	FILENAME="$2"
+	while read -r line; do
+		INSTANCEID="$line"
+		echo  	"$COMMAND $INSTANCEID Server"
+		aws ec2 $COMMAND --instance-ids $INSTANCEID
+	done < "$FILENAME"
+else
+	echo "Non valid server configuration file was supplied, please valid that you select a valid configuration file for the process.."
+fi
