fea68f2023139fb64452d4783c6a6a6db953a5d6
Roberto Carlos Trujillo Torres <robertotrujillo@MacBook-Pro-de-Roberto.local>
2018-11-08 00:20:54 -0600

    Added logic to instances.py

--- AWS/instances.py
+++ AWS/instances.py
@@ -1,26 +1,61 @@
 #!/usr/bin/python
 import sys
-import os.path
+import os
+import subprocess
 
+PARAMETER_ACTION 		     = 1;
+PARAMETER_CONFIG_FILE 	 = 2;
+PARAMETER_MINIMUM_ARGS	 = 3;
 
+def usage():
+	print "usage: python instances.py [action] [config-file]"
+	print "  	 [action] start,stop,state"
+	print "  	 [config] valid route to a config file"
+	print "Something like this :)! -> python instances.py start servers.txt"
+  	exit()
+	return 
 
 def doesThisFileExist(fileName):
+	return os.path.isfile(fileName)
 
-	return os.path.isfile(os.path.dirname(os.path.realpath(__file__))+"/"+fileName)
+def validateLengthInputArgs(argument):
+	if len(argument) < PARAMETER_ACTION:
+		return False
+	return True
+
+def assignActionValueForAWS(action):
+	if action == "start":
+		return "start-instances"
+	if action == "stop":
+		return "stop-instances"
+	if action == "state":
+		return "describe-instances"
+	usage()
+
+def assemblyAWSCommand(instanceId,command):
+	return "aws ec2 "+command+" --instance-ids "+ instanceId
+
+def executeCommand(command):
+	return subprocess.check_call((command),shell=True)	
+
+def executeAWSCommand(instanceId,command):
+	return executeCommand(assemblyAWSCommand(instanceId,command))
+
+if validateLengthInputArgs(sys.argv) == False:
+	usage()
+
+action = assignActionValueForAWS(str(sys.argv[PARAMETER_ACTION]))
+config = os.path.dirname(os.path.realpath(__file__))+"/" + str(sys.argv[PARAMETER_CONFIG_FILE])
+
+if doesThisFileExist(config)	==	False:
+	usage()
+
+with open(config) as f:
+ 	arguments_instances = f.readlines()
+  	map(lambda (instanceId) : executeAWSCommand(instanceId,action),arguments_instances)
 
 
-PARAMETER_ACTION 		= 1;
-PARAMETER_CONFIG_FILE 	= 2;
 
-if len(sys.argv)<3:
-	print "Non valid argument was supplied, please try again."
-	exit()
 
-action = str(sys.argv[PARAMETER_ACTION])
-config = str(sys.argv[PARAMETER_CONFIG_FILE])
 
-print 'ACTION',action ;
-print 'CONFIG',config ;
 
-print "Hello World!"
-print doesThisFileExist("servito.txt")
